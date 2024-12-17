02b8934d0fbedfe7f042aef84e08178fdc40c986
TopNiz <nizar.ayed@upgrade-code.org>
2024-12-17 16:37:37 +0100

    Completed articles service

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime, timezone
 from dataclasses import dataclass
 
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 # from pydantic import BaseModel
 from sqlmodel import Field, SQLModel, create_engine, Session, select
 
@@ -27,6 +27,12 @@ class Article(SQLModel, table=True):
     title: str
     body: str | None = Field(default=None)
     created_at: datetime | None = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+
+class ArticleUpdate(SQLModel):
+    author: str | None = None
+    title: str | None = None
+    body: str | None = None
 
 
 @app.get("/")
@@ -63,4 +69,55 @@ def list_articles():
         articles = session.exec(statement).all()
         return articles
         
+
+@app.get("/articles/{article_id}",
+         response_model=Article)
+def get_article(article_id: int):
+    with Session(myblog_db) as session:
+        article = session.get(Article, article_id)
+        if not article:
+            raise HTTPException(status_code=404, detail="Article not found")
+        return article
+    
+@app.delete("/articles/{article_id}")
+def delete_article(article_id: int):
+    with Session(myblog_db) as session:
+        article = session.get(Article, article_id)
+        if not article:
+            raise HTTPException(status_code=404, detail="Article not found")
+        session.delete(article)
+        session.commit()
+        return {
+            "deleted": True
+        }
+        
+# @app.put("/articles/") ==> need all the fields the object in one shot
+@app.patch("/articles/{article_id}", # will update some fields of the object
+           response_model=Article)
+def update_article(article_id: int, article: ArticleUpdate):
+    with Session(myblog_db) as session:
+        # Get the article from the database
+        article_db = session.get(Article, article_id)
+        
+        # Check if it is found
+        if not article_db:
+            raise HTTPException(status_code=404, detail="Article not found")
+        
+        # Map the update data with the fields from the database
+        article_data = article.model_dump(exclude_unset=True)
+        
+        # Effectively Update the article from the database
+        article_db.sqlmodel_update(article_data)
+        
+        # Mark this article as "Updatable"
+        session.add(article_db)
+        
+        # Commit the update to the database
+        session.commit()
+        
+        # Refresh the instance of the article of the session from the db
+        session.refresh(article_db)
+        
+        # Return it.
+        return article_db
     
